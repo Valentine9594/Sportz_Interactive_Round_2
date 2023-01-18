81cdaf7b074db44c8c385c30467e84bf8fa15c4a
Valentine Miranda <valentine.miranda@neosoftmail.com>
2023-01-18 18:48:32 +0530

    API calls

--- Sportz_Interactive_Round_2/Screen1ViewController/Screen1ViewController.swift
+++ Sportz_Interactive_Round_2/Screen1ViewController/Screen1ViewController.swift
@@ -8,7 +8,19 @@
 import UIKit
 
 class Screen1ViewController: UIViewController {
-
+    private var viewModel: Screen1ViewModelDelegate?
+    
+    init(viewModel: Screen1ViewModelDelegate) {
+        super.init(nibName: "Screen1ViewController", bundle: nil)
+        self.viewModel = viewModel
+    }
+    
+    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+        super.init(nibName: "Screen1ViewController", bundle: nil)
+        self.viewModel = nil
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
